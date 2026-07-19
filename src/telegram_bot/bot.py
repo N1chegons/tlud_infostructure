84cb959ba37b02d3f0e2e140cdf56ad313fe3ab4
N1chegons <krikignatenko@gmail.com>
2026-07-19 15:52:49 +0500

    update repository

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -103,7 +103,7 @@ async def register(call: CallbackQuery):
 
     registration_data[user_id] = {"step": "name"}
 
-    await bot.send_message(chat_id=call.message.chat.id, text="Ты еще не зарегестрирован, давай пройдем небольшую регистрацию!")
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ты еще не зарегестрирован, давай пройдем небольшую регистрацию!")
     await bot.send_message(chat_id=call.message.chat.id, text="Для начала, как тебя зовут? (Введите имя используя латиницу, пример: Ivan)")
 
 @bot.callback_query_handler(func=lambda call: call.data == "confirm")
