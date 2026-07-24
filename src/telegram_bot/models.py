ba7a0f44e72f7f79ba264a55e56762dfa89bdcc7
N1chegons <krikignatenko@gmail.com>
2026-07-24 22:15:22 +0500

    -

--- src/telegram_bot/models.py
+++ src/telegram_bot/models.py
@@ -39,7 +39,6 @@ class Consultation(Base):
     service_name: Mapped[str] = mapped_column(nullable=True)
     type: Mapped[ConsultationType] = mapped_column(default=ConsultationType.FREE)
     is_viewed: Mapped[bool] = mapped_column(default=False)
-    is_canceled: Mapped[bool] = mapped_column(default=False)
 
     created_at: Mapped[datetime.datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())")
@@ -54,6 +53,7 @@ class Service(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
     price: Mapped[float] = mapped_column(nullable=False)
+    is_canceled: Mapped[bool] = mapped_column(default=False)
 
     created_at: Mapped[datetime.datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())")
